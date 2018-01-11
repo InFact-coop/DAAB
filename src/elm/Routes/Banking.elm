8151a814593a52f60ee4182f8ff87494088b0d11
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-11 10:58:26 +0000

    adds banking deposit form to styling based on box styling for consistency realtes #19

--- src/elm/Routes/Banking.elm
+++ src/elm/Routes/Banking.elm
@@ -1,5 +1,6 @@
 module Routes.Banking exposing (..)
 
+import Components.SaveButton exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -8,6 +9,36 @@ import Types exposing (..)
 
 bankingRoute : Model -> Html Msg
 bankingRoute model =
-    div [ class "" ]
-        [ h1 [ class "tc f1" ] [ text "Banking" ]
+    let
+        partition =
+            span [ class "mh2" ] [ text " / " ]
+
+        max =
+            Html.Attributes.max
+
+        min =
+            Html.Attributes.min
+    in
+    section [ class "custom-padding" ]
+        [ Html.form []
+            [ h2 [ class "f3 b mt4" ] [ text "Banking Deposit form" ]
+            , div [ class "dib lh-copy" ]
+                [ label [ for "date", class "db f4" ] [ text "Date banked: " ]
+                , br [] []
+                ]
+            , div [ class "dib v-top ml3" ]
+                [ input [ id "date", type_ "date", class " sans-serif f4 b--grey db" ] []
+                , br [] []
+                ]
+            ]
+        , div [ class "dib lh-copy mt2" ]
+            [ label [ for "boxtotal", class "db f4" ] [ text "Amount in box: £" ]
+            ]
+        , div [ class "dib v-top ml3 mt2" ]
+            [ input [ placeholder "0.00", max "100000.00", min "0.00", step "0.01", type_ "number", class "db f4 w4" ] []
+            ]
+        , section [ class "actionButtons mt4" ]
+            [ saveButton AddBox "Save and add box"
+            , saveButton ExitBox "Save and exit"
+            ]
         ]
